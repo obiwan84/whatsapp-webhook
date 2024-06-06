4d314bc3f09584c768de89c2a27e805ff4cda2b8
Daniel Suarez <dsuarezm84@gmail.com>
2024-06-06 13:47:44 -0500

    Adding route for home

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello world, welcome to my MVP"
+    
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     return "Hello WhatsApp"
