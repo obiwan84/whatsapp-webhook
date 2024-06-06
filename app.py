421a5104c85ddddab2ed72e4287d3ba59598ab50
Daniel Suarez <dsuarezm84@gmail.com>
2024-06-06 15:28:15 -0500

    Adding app port

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return "Hello world, welcome to my MVP"
-    
+
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     return "Hello WhatsApp"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
